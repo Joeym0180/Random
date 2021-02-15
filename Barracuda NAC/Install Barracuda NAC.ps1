e2c943080fe7aa68a37ea83773956d5dc905c28f
Joeym0180 <meijer.j98@gmail.com>
2021-02-15 14:13:13 +0100

    addedurl

--- Barracuda NAC/Install Barracuda NAC.ps1
+++ Barracuda NAC/Install Barracuda NAC.ps1
@@ -4,9 +4,10 @@ function Install-NAC {
     $Logfile = "C:\Temp\NACInstall.log"
     Start-Transcript -Path $Logfile
     $url = 'https://github.com/Joeym0180/Random/raw/main/Installatiebestanden/BarracudaNAC.msi'
+    $url = 'http://cargospot.globalgsagroup.com/cargospot/barracudavpnfiles/BarracudaNAC.msi'
     $msi = 'C:\Temp\BarracudaNAC.msi'
     invoke-webrequest $url -OutFile $msi
-    msiexec.exe /i $msi /qn /norestart
+    msiexec.exe /i $msi /qn /norestart 
     Stop-Transcript
 } #function
 Install-NAC
